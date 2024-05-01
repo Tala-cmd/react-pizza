66806696e83746e33c0960655add82f587b776c1
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-05-02 00:42:18 +0400

    Styling Order Form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 
 function Button({ children, disabled, to, type }) {
   
-  const base = 'inline-block font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed'
+  const base = 'inline-block text-sm font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed'
   
   const styles={
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-6 md:py-2.5 text-xs',
-    secondary: ' px-4 py-2.5 md:px-6 md:py-3.5 inline-block font-semibold tracking-wide uppercase transition-colors duration-300 border border-stone-300 rounded-full text-stone-400 hover:bg-stone-300 focus:ring focus:ring-stone-200 focus:text-stone-800 hover:text-stone-800 focus:bg-stone-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed'
+    secondary: ' px-4 py-2.5 text-sm md:px-6 md:py-3.5 inline-block font-semibold tracking-wide uppercase transition-colors duration-300 border border-stone-300 rounded-full text-stone-400 hover:bg-stone-300 focus:ring focus:ring-stone-200 focus:text-stone-800 hover:text-stone-800 focus:bg-stone-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed'
   }
   
   if(to) return <Link to={to} className={styles[type]}>{children}</Link>

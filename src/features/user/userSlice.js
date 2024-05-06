08a54dc7a6db78c221ef24a3fca559969d598435
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-05-06 13:06:07 +0400

    Reading and updating the user state

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -23,7 +23,7 @@ async function fetchAddress() {
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-  username:'Tala'
+  username:''
 }
 
 const userSlice = createSlice({
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((store)=> store.user.username)
+  
   return (
     <div className="px-4 my-10 text-center sm:my-16">
       <h1 className="mb-8 text-xl font-semibold md:text-3xl">
@@ -8,8 +12,8 @@ function Home() {
         <br />
         <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
       </h1>
+      {!username ? <CreateUser /> : <Button type='primary' to='/menu'>Continue Ordering, {username}</Button>}
       
-      <CreateUser />
     </div>
   );
 }

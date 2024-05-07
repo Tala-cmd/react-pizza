e8f23dfdbdeff385f65c158e8411eb89f13dd194
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-05-07 22:15:39 +0400

    Deleting cart items

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,16 @@
 import { formatCurrency } from "../../utilities/helpers";
 import Button from '../../ui/Button'
-import { useDispatch } from "react-redux";
-import { addItem } from "../cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-
+  
   const dispatch = useDispatch()
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id))
+  const isInCart = currentQuantity > 0;
   
   function handleAddToCart(){
     const newItem ={
@@ -18,6 +22,7 @@ function MenuItem({ pizza }) {
     }
 
     dispatch(addItem(newItem))
+    
   }
   
   return (
@@ -29,11 +34,14 @@ function MenuItem({ pizza }) {
         <div className="flex items-center justify-between mt-auto">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm font-medium uppercase text-stone-500">Sold out</p>}
         
-        {!soldOut && <Button type='small' onClick={handleAddToCart}>Add to cart</Button>}
+          {isInCart && <DeleteItem pizzaId={id} />}
+        
+        {!soldOut && !isInCart &&
+          <Button type='small' onClick={handleAddToCart}>Add to cart</Button>}
         </div>
       </div>
     </li>
   );
 }
 
-export default MenuItem;
+export default MenuItem; 
